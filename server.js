11ca21cb55c4779e07c43b96603f0b39629bbeb8
mirzabaig <baig.zee@hotmail.com>
2023-06-28 16:52:42 +0930

    error handling to deploy

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const sessions = require('./middlewares/sessions')
 const usersController =require('./controllers/users_controller')
 const sessionsController = require('./controllers/sessions_controller')
 const profilesController = require('./controllers/profiles_controller')
+
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path')
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+
 // Define a route
 app.get('/api/message', (req, res) => {
   res.json({ message: 'Hello from the server4545434545!' });
@@ -23,12 +34,5 @@ app.use('/api/profiles', profilesController)
 app.use('/api/users', usersController)
 app.use('/api/sessions', sessionsController)
 
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path')
-  app.use(express.static(path.join(__dirname, 'build')));
 
-  app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
 
